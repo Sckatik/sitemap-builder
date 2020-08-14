454f444051927ecdd6cbf8e25533692661ddedba
Dmitrii Simakov <sckat@yandex.ru>
2020-08-14 11:54:26 +0300

    Update web.php
    Убрал старый код

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,3 @@ use Illuminate\Support\Facades\Route;
 use Dsimakov\Sitemap\Controllers\SitemapController;
 
 Route::get('/sitemap.xml', [SitemapController::class, 'index']);
-
-
-/*function () {
-    $value = config('main.menu');
-    dd($value);
-    return 'package_works1!';
-}
-*/
